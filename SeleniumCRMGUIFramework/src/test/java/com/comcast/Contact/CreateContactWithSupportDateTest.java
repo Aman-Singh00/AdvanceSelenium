6f8670bc6ce30ad0201b354628c70467d1996d09
ak683888 <saman746265@gmail.com>
2023-09-08 16:41:42 +0530

    Last commit

--- SeleniumCRMGUIFramework/src/test/java/com/comcast/Contact/CreateContactWithSupportDateTest.java
+++ SeleniumCRMGUIFramework/src/test/java/com/comcast/Contact/CreateContactWithSupportDateTest.java
@@ -9,35 +9,34 @@ import com.comcast.crm.objectrepositoryutility.ContactPage;
 import com.comcast.crm.objectrepositoryutility.CreatingNewContactPage;
 import com.comcast.crm.objectrepositoryutility.HomePage;
 
-public class CreateContactWithSupportDateTest  extends BaseClass{
-	
+public class CreateContactWithSupportDateTest extends BaseClass {
+
 	@Test(groups = "RegressionTest")
-	public void contactWithSupportDate() throws Throwable
-	{
+	public void contactWithSupportDate() throws Throwable {
 		// read testScritp data from Excel file
-				String lastName = excel.getDataFromExcel("contact", 4, 2) + jLib.getRandomNumber();
+		String lastName = excel.getDataFromExcel("contact", 4, 2) + jLib.getRandomNumber();
 
-				// step 2 : navigate to Contact module
-				HomePage hp = new HomePage(driver);
-				hp.getContactlnk().click();
+		// step 2 : navigate to Contact module
+		HomePage hp = new HomePage(driver);
+		hp.getContactlnk().click();
 
-				// step 3 : click on "create Contact" Button
-				ContactPage cp = new ContactPage(driver);
-				cp.getCreateNewOrgBtn().click();
+		// step 3 : click on "create Contact" Button
+		ContactPage cp = new ContactPage(driver);
+		cp.getCreateNewOrgBtn().click();
 
-				// step 4 : enter all the details & create new Contact
-				String endDate = jLib.getSystemDateYYYYDDMM();
-				String startDate = jLib.getRequriedDateYYYYDDMM(30);
-				CreatingNewContactPage ccp = new CreatingNewContactPage(driver);
-				ccp.createContactWithSupportDate(lastName, startDate, endDate);
+		// step 4 : enter all the details & create new Contact
+		String endDate = jLib.getSystemDateYYYYDDMM();
+		String startDate = jLib.getRequriedDateYYYYDDMM(30);
+		CreatingNewContactPage ccp = new CreatingNewContactPage(driver);
+		ccp.createContactWithSupportDate(lastName, startDate, endDate);
 
-				// verify Header phone Number info Expected Result
-				ContactInfoPage cip = new ContactInfoPage(driver);
-				String actStartDate = cip.getSuppoortStartDateInfo().getText();
-				Assert.assertEquals(actStartDate.trim(), startDate);
-				
-				String actendDate = cip.getSupportEndDateInfo().getText();
-				Assert.assertEquals(actendDate.trim(), endDate);
+		// verify Header phone Number info Expected Result
+		ContactInfoPage cip = new ContactInfoPage(driver);
+		String actStartDate = cip.getSuppoortStartDateInfo().getText();
+		Assert.assertEquals(actStartDate.trim(), startDate);
+
+		String actendDate = cip.getSupportEndDateInfo().getText();
+		Assert.assertEquals(actendDate.trim(), endDate);
 	}
 
 }
