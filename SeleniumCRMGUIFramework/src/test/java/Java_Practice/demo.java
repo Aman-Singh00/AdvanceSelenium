718cab26fc9928b35d668b03c17d61e11d5a9cb7
ak683888 <saman746265@gmail.com>
2023-09-08 16:25:00 +0530

    last push

--- SeleniumCRMGUIFramework/src/test/java/Java_Practice/demo.java
+++ SeleniumCRMGUIFramework/src/test/java/Java_Practice/demo.java
@@ -1,10 +1,26 @@
 package Java_Practice;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class demo {
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://shoppersstack.com/user-signin");
+		driver.manage().window().maximize();
 	
 		
+		driver.findElement(By.xpath("//input[@name='Email']")).sendKeys("abcd");
+		
+		//driver.findElement(By.xpath("//span[text()='Login']")).click();
+
+		// System.out.println(email.getAttribute("title"));
+		driver.quit();
+
 	}
 
 }
